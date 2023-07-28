3bed5c5974c59f82bc7260aca49a07dc0d1175cc
yunwei37 <1067852565@qq.com>
2023-07-28 23:22:44 +0800

    add x3 example

---
+++ z3-example/minimal.py
@@ -0,0 +1,21 @@
+from z3 import *
+
+def verify_equation():
+    x = Int('x')  # Declare an integer variable 'x'
+
+    # Declare the equation 'x + 2 = 5'
+    equation = x + 2 == 5
+
+    # Create a solver and add the equation to it
+    solver = Solver()
+    solver.add(equation)
+
+    # Check for satisfiability
+    if solver.check() == sat:
+        # If satisfiable, print the model (i.e., solution for 'x')
+        model = solver.model()
+        print("Solution for x:", model[x])
+    else:
+        print("No solution found.")
+
+verify_equation()
