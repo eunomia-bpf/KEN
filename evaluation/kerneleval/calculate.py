37adf9fb8a0d67d8ee76b3833cc4b45e667b27da
victoryang00 <yangyiwei2000@gmail.com>
2023-09-27 16:04:47 -0700

    update cal part

--- evaluation/kerneleval/calculate.py
+++ evaluation/kerneleval/calculate.py
@@ -12,50 +12,85 @@ def clone_raviole_kernel():
     os.system("git clone https://android.googlesource.com/device/google/raviole-kernel/")
 
 # pixel
-def get_kprobe_pixel_kernel():
+def get_kprobe_pixel_kernel()->int:
     os.chdir("pixel_kernel")
     res = subprocess.run(["git log|grep kprobe|wc -l"], stdout=subprocess.PIPE)
     return int(res.stdout.decode("utf-8").split(" ")[0])
 
-def get_tracepoint_pixel_kernel():
+def get_kretprobe_pixel_kernel()->int:
+    os.chdir("pixel_kernel")
+    res = subprocess.run(["git log|grep kretprobe|wc -l"], stdout=subprocess.PIPE)
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+
+def get_tracepoint_pixel_kernel()->int:
     os.chdir("pixel_kernel")
     res = subprocess.run(["git log|grep tracepoint|wc -l"], stdout=subprocess.PIPE)
     return int(res.stdout.decode("utf-8").split(" ")[0])
 
-def get_seccomp_pixel_ebpf_rules():
-    pass
+def get_cgroup_pixel_ebpf_rules()->int:
+    os.chdir("pixel_kernel")
+    res = subprocess.run(["git log|grep cgroup|wc -l"], stdout=subprocess.PIPE)
+    return int(res.stdout.decode("utf-8").split(" ")[0])
 
-def get_cgroup_pixel_ebpf_rules():
-    pass
+def get_application_pixel_kernel()->int:
+    # how to parse the pixel
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_syscall_pixel_kernel()->int:
+    # how to parse the pixel
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_network_pixel_kernel()->int:
+    # how to parse the pixel
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_fs_pixel_ebpf_rules()->int:
+    # how to parse the pixel
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_memory_pixel_ebpf_rules()->int:
+    # how to parse the pixel
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_cpu_pixel_ebpf_rules()->int:
+    # how to parse the pixel
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
 
 # greggs brandon
-def get_kprobe_brandon_dataset():
+def get_kprobe_brandon_dataset()->int:
+    os.chdir("../submodules/ebpf-syscall-dataset")
+    res = subprocess.run(["git log|grep kprobe|wc -l"], stdout=subprocess.PIPE)
+
+def get_tracepoint_brandon_dataset()->int:
     pass
 
-def get_tracepoint_brandon_dataset():
+def get_seccomp_brandon_ebpf_rules()->int:
     pass
 
-def get_seccomp_brandon_ebpf_rules():
-    pass
-
-def get_cgroup_brandon_ebpf_rules():
+def get_cgroup_brandon_ebpf_rules()->int:
     pass
 
 # ebpfnldataset
-def get_kprobe_ebpfnl_dataset():
+def get_kprobe_ebpfnl_dataset()->int:
     pass
 
-def get_tracepoint_ebpfnl_dataset():
+def get_tracepoint_ebpfnl_dataset()->int:
     pass
 
-def get_seccomp_ebpfnl_ebpf_rules():
+def get_seccomp_ebpfnl_ebpf_rules()->int:
     pass
 
-def get_cgroup_ebpfnl_ebpf_rules():
+def get_cgroup_ebpfnl_ebpf_rules()->int:
+    pass
+
+def plot_graph():
     pass
 
 if __name__ == "__main__":
     if not os.path.exists("pixel_kernel"):
         clone_pixel_kernel()
-
+    if not os.path.exists("raviole-kernel"):
+        clone_raviole_kernel()
     calculate_pixel_ebpf_rules()
--- evaluation/performace/performance.py
+++ evaluation/performace/performance.py
@@ -2,7 +2,11 @@ import argparse
 import GPUtil
 import pynvml
 import psutil
+import time
 
+def start_magic_trace():
+    os.system("./magic")
+    pass
 
 def get_vram(desired_pid) -> int:
     # Fetching GPU details
@@ -30,14 +34,16 @@ def get_vram_nvml(desired_pid) -> int:
 
     # Get the handle for the first GPU device
     device_handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-
+    mem = 0
     try:
         # Get the list of compute running processes on the GPU
         processes = pynvml.nvmlDeviceGetComputeRunningProcesses(device_handle)
         
         for process in processes:
+            if process.process_name.__contains__("python"):
+                mem += process.usedGpuMemory
             print(f'PID: {process.pid}, Process Name: {process.process_name}, Used GPU Memory: {process.usedGpuMemory}')
-
+        return mem
     except pynvml.NVMLError as error:
         print(f"Failed to retrieve processes: {error}")
 
@@ -50,27 +56,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get GPU memory usage for a given PID")
     parser.add_argument("--pid", type=int, required=False)
     args = parser.parse_args()
-    desired_pid = args.pid
     if args.pid is None:
-        print("No PID given exiting...")
-        exit(0)
+        pid=start_magic_trace()
     else:
-        try:
-            pynvml.nvmlInit()
-            process = psutil.Process(desired_pid)
-            with open(f"usage_{desired_pid}.csv", "w") as f:
-                # Poll the process status in a loop
-                while process.is_running():
-                    # get_vram(args.pid)
-                    vram = get_vram_nvml(desired_pid)
-                    
-                    time.sleep(1)  # sleep for 1 second
-            
-            print(f"Process {desired_pid} has finished.")
+        desired_pid = args.pid
 
-        except psutil.NoSuchProcess:
-            print(f"No process with PID {desired_pid} is running.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    try:
+        pynvml.nvmlInit()
+        process = psutil.Process(desired_pid)
+        with open(f"usage_{desired_pid}.csv", "w") as f:
+            # Poll the process status in a loop
+            while process.is_running():
+                # get_vram(args.pid)
+                vram = get_vram_nvml(desired_pid)
+                
+                time.sleep(1)  # sleep for 1 second
+        
+        print(f"Process {desired_pid} has finished.")
+
+    except psutil.NoSuchProcess:
+        print(f"No process with PID {desired_pid} is running.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
     pynvml.nvmlShutdown()
     
