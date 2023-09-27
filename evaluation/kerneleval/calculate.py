9cf7fd7e9b11a3d441acc4dc582e0f6c65964df1
victoryang00 <yangyiwei2000@gmail.com>
2023-09-26 18:44:37 -0700

    add calcuation

--- evaluation/kerneleval/calculate.py
+++ evaluation/kerneleval/calculate.py
@@ -1,9 +1,61 @@
 import os
+import subprocess
 # https://source.android.com/docs/core/architecture/kernel/bpf
 def clone_pixel_kernel():
+    os.system("mkdir -p pixel_kernel")
+    os.chdir("pixel_kernel")
     os.system("repo init -u https://android.googlesource.com/kernel/manifest -b android-gs-raviole-5.10-android13-qpr2")
     os.system("repo sync -j$(nproc --all)")
 
-def calculate_pixel_ebpf_rules():
+# https://source.android.com/docs/core/architecture/kernel/bpf
+def clone_raviole_kernel():
+    os.system("git clone https://android.googlesource.com/device/google/raviole-kernel/")
+
+# pixel
+def get_kprobe_pixel_kernel():
+    os.chdir("pixel_kernel")
+    res = subprocess.run(["git log|grep kprobe|wc -l"], stdout=subprocess.PIPE)
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_tracepoint_pixel_kernel():
+    os.chdir("pixel_kernel")
+    res = subprocess.run(["git log|grep tracepoint|wc -l"], stdout=subprocess.PIPE)
+    return int(res.stdout.decode("utf-8").split(" ")[0])
+
+def get_seccomp_pixel_ebpf_rules():
     pass
 
+def get_cgroup_pixel_ebpf_rules():
+    pass
+
+# greggs brandon
+def get_kprobe_brandon_dataset():
+    pass
+
+def get_tracepoint_brandon_dataset():
+    pass
+
+def get_seccomp_brandon_ebpf_rules():
+    pass
+
+def get_cgroup_brandon_ebpf_rules():
+    pass
+
+# ebpfnldataset
+def get_kprobe_ebpfnl_dataset():
+    pass
+
+def get_tracepoint_ebpfnl_dataset():
+    pass
+
+def get_seccomp_ebpfnl_ebpf_rules():
+    pass
+
+def get_cgroup_ebpfnl_ebpf_rules():
+    pass
+
+if __name__ == "__main__":
+    if not os.path.exists("pixel_kernel"):
+        clone_pixel_kernel()
+
+    calculate_pixel_ebpf_rules()
---
+++ evaluation/performace/performance.py
@@ -0,0 +1,76 @@
+import argparse
+import GPUtil
+import pynvml
+import psutil
+
+
+def get_vram(desired_pid) -> int:
+    # Fetching GPU details
+    GPUs = GPUtil.getGPUs()
+    for i, gpu in enumerate(GPUs):
+        print(f"GPU {i} : {gpu.name}")
+
+        # Fetching GPU processes using nvidia-smi
+        gpu_processes = GPUtil.getGPUs()[i].processes
+        
+        # Check each process on the current GPU
+        for p in gpu_processes:
+            if p.pid == desired_pid:
+                try:
+                    proc = psutil.Process(p.pid)
+                    print(f"Process ID: {p.pid}")
+                    print(f"GPU Memory Used: {p.gpu_memory_usage}MB")
+                    print(f"Process Name: {proc.name()}")
+                    print(f"User: {proc.username()}")
+                except psutil.NoSuchProcess as e:
+                    print(f"Process with PID {p.pid} not found.")
+            return p.gpu_memory_usage
+
+def get_vram_nvml(desired_pid) -> int:
+
+    # Get the handle for the first GPU device
+    device_handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+
+    try:
+        # Get the list of compute running processes on the GPU
+        processes = pynvml.nvmlDeviceGetComputeRunningProcesses(device_handle)
+        
+        for process in processes:
+            print(f'PID: {process.pid}, Process Name: {process.process_name}, Used GPU Memory: {process.usedGpuMemory}')
+
+    except pynvml.NVMLError as error:
+        print(f"Failed to retrieve processes: {error}")
+
+
+def get_ps(desired_pid) ->(float, float):
+    output= subprocess.run(["ps", "-p", desired_pid, "-o", "%cpu,%mem"]).stdout.decode("utf-8").split("\n")[1].split(" ")
+    return float(output[0]), float(output[1])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get GPU memory usage for a given PID")
+    parser.add_argument("--pid", type=int, required=False)
+    args = parser.parse_args()
+    desired_pid = args.pid
+    if args.pid is None:
+        print("No PID given exiting...")
+        exit(0)
+    else:
+        try:
+            pynvml.nvmlInit()
+            process = psutil.Process(desired_pid)
+            with open(f"usage_{desired_pid}.csv", "w") as f:
+                # Poll the process status in a loop
+                while process.is_running():
+                    # get_vram(args.pid)
+                    vram = get_vram_nvml(desired_pid)
+                    
+                    time.sleep(1)  # sleep for 1 second
+            
+            print(f"Process {desired_pid} has finished.")
+
+        except psutil.NoSuchProcess:
+            print(f"No process with PID {desired_pid} is running.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    pynvml.nvmlShutdown()
+    
